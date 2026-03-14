4953e02848ec2bba5f9e68eb96d6179959b61b6e
Thorsten Ludewig <t.ludewig@gmail.com>
2026-03-14 14:29:32 +0100

    demo scripts

--- docker/mousetrap/GENERATE_TOKEN.sh
+++ docker/mousetrap/GENERATE_TOKEN.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker run --rm \
-  -v ../data:/data:ro \
-  -it ghcr.io/thorsten-l/l9g-uidgen:latest -g
+  -v ./data:/data:ro \
+  -v ./l9g-mousetrap.jar:/l9g-mousetrap.jar:ro \
+   bellsoft/liberica-openjdk-alpine:21 \
+   java -jar /l9g-mousetrap.jar -g
